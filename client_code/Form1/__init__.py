cc9896296b2a60a67a277e78aefe7ec85b627915
thal01veloso@gmail.com <thal01veloso@gmail.com>
2023-11-05 02:21:22 +0000

    Created Form1.ItemTemplate1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,19 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.tarefas.search()
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    result = app_tables.tarefas.add_row(tarefa=self.tarefas.text,Status=True)
+    self.refresh_data_bindings()
+
